bcfd0d0a03a1f89e791c725bf2e2920a577b320d
hxmblevishu <dhimnvishnu@gmail.com>
2026-06-15 10:52:54 +0530

    chore: prepare for deployment

--- src/main/java/com/devtinder/dto/request/ChatMessageRequest.java
+++ src/main/java/com/devtinder/dto/request/ChatMessageRequest.java
@@ -1,11 +1,12 @@
 package com.devtinder.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ChatMessageRequest(
-        @NotBlank
-        @Size(max = 1200)
-        String content
+        @NotNull Long matchId,
+        String senderEmail,
+        @NotBlank @Size(max = 1200) String content
 ) {
 }
