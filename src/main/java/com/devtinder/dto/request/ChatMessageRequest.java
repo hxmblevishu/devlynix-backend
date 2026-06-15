b207a11f1e67d7703c7a490c3bf965ff6c326f99
hxmblevishu <dhimnvishnu@gmail.com>
2026-06-15 05:52:48 +0530

    Fix chat message validation

--- src/main/java/com/devtinder/dto/request/ChatMessageRequest.java
+++ src/main/java/com/devtinder/dto/request/ChatMessageRequest.java
@@ -1,12 +1,11 @@
 package com.devtinder.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ChatMessageRequest(
-        @NotNull Long matchId,
-        String senderEmail,
-        @NotBlank @Size(max = 1200) String content
+        @NotBlank
+        @Size(max = 1200)
+        String content
 ) {
 }
